3e70c6115619c6d6efb8bece0593f5d26b0a08ed
Dolma <dolmatgurung1@gmail.com>
2024-09-23 16:33:17 -0400

    setup scripts added

---
+++ scripts/setup.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+ssh -i "workload_4.pem" ubuntu@10.0.132.139 'bash ~/start_app.sh'
---
+++ scripts/start_app.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt install -y python3.9 python3.9-venv python3-pip nginx git
+# Clone the GitHub repository if it doesn't already exist
+if [ ! -d "microblog_VPC_deployment" ]; then
+  git clone https://github.com/dolmagrg123/microblog_VPC_deployment.git
+fi
+cd microblog_VPC_deployment
+python3.9 -m venv venv
+echo "Activating vitual env"
+source venv/bin/activate
+echo "Installing Requirements of the application"
+pip install -r requirements.txt
+pip install gunicorn pymysql cryptography
+export FLASK_APP=microblog.py
+flask translate compile
+flask db upgrade
+echo "Starting Gunicorn..."
+gunicorn -b :5000 -w 4 microblog:app --daemon
