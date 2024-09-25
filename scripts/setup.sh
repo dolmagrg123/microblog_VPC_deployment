420cf5a3c37979696e5df4c75da685f2932ccc04
Dolma <dolmatgurung1@gmail.com>
2024-09-25 16:25:33 -0400

    start_app script url updated

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,9 @@ APPLICATION_SERVER_IP=$1
 chmod 400 workload_4.pem
 
 # SSH into the application server and run start_app.sh
-ssh -i "workload_4.pem" ubuntu@${APPLICATION_SERVER_IP} 'curl -o https://github.com/dolmagrg123/microblog_VPC_deployment/blob/main/scripts/start_app.sh && bash ~/start_app.sh'
+echo "logging into application server and running start_app script"
+ssh -i "workload_4.pem" ubuntu@${APPLICATION_SERVER_IP} 'curl -o ~/start_app.sh https://raw.githubusercontent.com/dolmagrg123/microblog_VPC_deployment/main/scripts/start_app.sh && bash ~/start_app.sh'
+
 if [ $? -ne 0 ]; then
     echo "Failed to execute start_app.sh on ${APPLICATION_SERVER_IP}"
     exit 1
