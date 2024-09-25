4ed445a847e9d4a36795038b95ca0e018e56bc19
Dolma <dolmatgurung1@gmail.com>
2024-09-25 16:15:50 -0400

    using curl to download script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,7 @@ APPLICATION_SERVER_IP=$1
 chmod 400 workload_4.pem
 
 # SSH into the application server and run start_app.sh
-ssh -i "workload_4.pem" ubuntu@${APPLICATION_SERVER_IP} 'bash curl -o https://github.com/dolmagrg123/microblog_VPC_deployment/blob/main/scripts/start_app.sh''bash ~/start_app.sh'
+ssh -i "workload_4.pem" ubuntu@${APPLICATION_SERVER_IP} 'curl -o https://github.com/dolmagrg123/microblog_VPC_deployment/blob/main/scripts/start_app.sh && bash ~/start_app.sh'
 if [ $? -ne 0 ]; then
     echo "Failed to execute start_app.sh on ${APPLICATION_SERVER_IP}"
     exit 1
